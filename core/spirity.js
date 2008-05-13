6eb28463dbf1d7c2cbf93a9f1ab6ef50158027ee
i.feelinglucky <i.feelinglucky@ba9af86f-184d-0410-86f9-ad65c3e6983c>
2008-05-13 08:55:03 +0000

    初始化目录，增加基本的 Javascript 文件

---
+++ core/spirity.js
@@ -0,0 +1,29 @@
+// vim: set et sw=4 ts=4 sts=4 fdm=marker ff=unix fenc=utf8
+/**
+ * Spirity Javascript Framework
+ *
+ * @author feeinglucky<i.feelinglucky@gmail.com>
+ * @link   http://www.gracecode.com/
+ */
+
+if (typeof Spirity == "undefined" || !Spirity) {
+    var Spirity = {};
+}
+
+
+Spirity.extend = function() {
+    var a = arguments, o = Spirity;
+    for (var i = 0; i < a.length; i++) {
+        var tmp = a[i].split(".");
+
+        for (var j = (tmp[0] == "Spirity" ? 1 : 0); j < tmp.length; j++) {
+            o[tmp[j]] = o[tmp[j]] || {};
+            o = o[tmp[j]];
+        }
+    }
+
+    return o;
+};
+
+
+
